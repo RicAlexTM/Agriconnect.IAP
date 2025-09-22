40e35aaa4b11d11aa3858ced210694ea0121bb04
godfreyM04 <gomark026@gmail.com>
2025-09-22 08:47:42 +0300

    Change wordings in farmers.php

--- farmers.php
+++ farmers.php
@@ -28,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Farmer Profile</title>
+    <title>Farmer's Profile</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
