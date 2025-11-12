982ca1fba8defb0cdb7c1994999ef328ad5bbcd3
kadaji777 <162417406+kadaji777@users.noreply.github.com>
2025-11-12 10:20:14 +0300

    Fix indentation in register.blade.php

--- AgriconnectKE/resources/views/auth/register.blade.php
+++ AgriconnectKE/resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/auth/register.blade.php -->
 @extends('layouts.app')
 
-@section('content')
+ @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
